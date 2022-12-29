f8720fd1fb31697f797b982ece135467701589ae
Marios Georgiou <mgeorgiou.ece@gmail.com>
2022-12-29 12:18:01 -0500

    cron works

--- trader.py
+++ trader.py
@@ -1,3 +1,4 @@
+import json
 from coinbase.wallet.client import Client
 import os
 import logging
@@ -9,43 +10,27 @@ logging.basicConfig(
 
 
 def main():
-    api_key = os.getenv("COINBASE_API_KEY")
-    api_secret = os.getenv("COINBASE_API_SECRET")
+    # TODO: Load the keys to the docker container as environment variables using --build-arg
+    # api_key = os.getenv("COINBASE_API_KEY")
+    # api_secret = os.getenv("COINBASE_API_SECRET")
+
+    # Open the keys file
+    with open(".keys", "r") as f:
+        keys = json.load(f)["coinbase"]
+
+    api_key = keys["COINBASE_API_KEY"]
+    api_secret = keys["COINBASE_API_SECRET"]
 
     client = Client(api_key, api_secret)
 
-    rates = client.get_exchange_rates()["rates"]
-    logging.info(f"Bitcoin's price is {rates['BTC']}")
+    account = client.get_primary_account()
 
-    # payment_method = client.get_payment_methods()[0]
-    # logging.info(payment_method)
-
-    # account = client.get_primary_account()
-    # logging.info(account)
-
-    # accounts = client.get_accounts()
-    # print(accounts)
-
-    # usd_account = client.get_account('881be368-8c5a-58d3-aa25-515a5763c51b')
-    # print(usd_account['id'])
-
-    primary = client.get_primary_account()
-    print(primary['id'])
-
-    order = primary.buy(
+    order = account.buy(
         amount="2",
         currency="USD",
     )
 
-    print(order)
-
-    # order = client.buy(
-    #     amount="0.0001",
-    #     account_id="881be368-8c5a-58d3-aa25-515a5763c51b",
-    #     paymemt_method="d9627fbb-333e-583e-b2f5-efcfeeab6c95",
-    #     currency="BTC",
-    # )
-    # logging.info(f"order is {order}")
+    logging.info(f"order details: {order['status']}")
 
 
 if __name__ == "__main__":
